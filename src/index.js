d1273c9f5a04203953720158ef3d0b13ce920231
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-09-28 23:06:03 +0300

    Local host test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello express");
+});
+
+app.listen(PORT, () => console.log(`Server is runing on port ${PORT}...`));
