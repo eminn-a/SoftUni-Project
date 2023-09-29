a36e02839308c991b0478795ba6ca7b17a1bf6f4
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-09-29 13:20:47 +0300

    HomeController

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ const express = require("express");
 
 const expressConfig = require("./config/expressConfig");
 const handlebarsConfig = require("./config/handlebarsConfig");
+const homeController = require("./controllers/homeController");
 
 const app = express();
 const PORT = 5000;
 
 //Express congig
 expressConfig(app);
-
 //Handlebars confing
 handlebarsConfig(app);
+
 //Routes
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(homeController);
 
 app.listen(PORT, () => console.log(`Server is runing on port ${PORT}...`));
