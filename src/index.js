cee895e9309022841baa07b770722d2bc7e8f94e
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-09-29 12:13:16 +0300

    Added some CSS and main page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const PORT = 5000;
 
+//Express congig
+app.use(express.static("src/public"));
+
+//Handlebars confing
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+//Routes
 app.get("/", (req, res) => {
-  res.send("Hello express");
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server is runing on port ${PORT}...`));
