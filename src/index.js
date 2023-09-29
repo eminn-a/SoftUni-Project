bda55f297da2ff80b80aac5b99428a1fbb57cd2e
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-09-29 12:58:31 +0300

    Added express and handlebars confg

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
+
+const expressConfig = require("./config/expressConfig");
+const handlebarsConfig = require("./config/handlebarsConfig");
 
 const app = express();
 const PORT = 5000;
 
 //Express congig
-app.use(express.static("src/public"));
+expressConfig(app);
 
 //Handlebars confing
-app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
-app.set("views", "src/views");
-
+handlebarsConfig(app);
 //Routes
 app.get("/", (req, res) => {
   res.render("index");
