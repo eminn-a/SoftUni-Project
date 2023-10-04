8dc93e978876f5e3e0d2fdf27728f63bcbadf1ae
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-10-04 21:20:02 +0300

    Added accesory create page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController");
 const cubeController = require("../src/controllers/cubeController");
+const accesoryController = require("../src/controllers/accessoryController");
 
 router.use(homeController);
 router.use("/cubes", cubeController);
+router.use("/accessories", accesoryController);
 router.use("*", (req, res) => {
   res.redirect("/404");
 });
