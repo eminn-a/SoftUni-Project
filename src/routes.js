7144566bed5119a717c72fc5904d65003f138e02
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-10-12 11:29:09 +0300

    Add user navi bar tools and working register page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController");
 const cubeController = require("../src/controllers/cubeController");
 const accesoryController = require("../src/controllers/accessoryController");
+const userController = require("../src/controllers/userController");
 
 router.use(homeController);
 router.use("/cubes", cubeController);
 router.use("/accessories", accesoryController);
+router.use("/user", userController);
 router.use("*", (req, res) => {
   res.redirect("/404");
 });
