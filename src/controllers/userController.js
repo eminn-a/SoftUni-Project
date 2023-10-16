07b619fda80d2afed700c1bce7c3f88797e5380e
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-10-16 19:29:51 +0300

    Add validations and allover error handling

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userManager = require("../manager/userManager");
+const { extractErrorMessages } = require("../utils/errorHelpers");
 
 router.get("/register", (req, res) => {
   res.render("users/registerPage");
@@ -8,9 +9,13 @@ router.get("/register", (req, res) => {
 router.post("/register", async (req, res) => {
   const { username, password, repeatPassword } = req.body;
 
-  await userManager.register({ username, password, repeatPassword });
-
-  res.redirect("/users/login");
+  try {
+    await userManager.register({ username, password, repeatPassword });
+    res.redirect("/users/login");
+  } catch (err) {
+    const errorMessages = extractErrorMessages(err);
+    res.render("users/registerPage", { errorMesages: errorMessages });
+  }
 });
 
 router.get("/login", (req, res) => {
@@ -20,10 +25,15 @@ router.get("/login", (req, res) => {
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
-  const token = await userManager.login(username, password);
-
-  res.cookie("auth", token, { httpOnly: true });
-  res.redirect("/");
+  try {
+    const token = await userManager.login(username, password);
+    res.cookie("auth", token, { httpOnly: true });
+    res.redirect("/");
+  } catch (err) {
+    const errorMessages = extractErrorMessages(err);
+    console.log(errorMessages);
+    res.render("users/loginPage", { errorMesages: errorMessages });
+  }
 });
 
 router.get("/logout", (req, res) => {
--- src/models/User.js
+++ src/models/User.js
@@ -4,14 +4,14 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
-    minLength: [5, "Password is too short"],
-    match: /^[A-Za-z0-9]+$/,
+    required: [true, "Username is required!"],
+    minLength: [5, "Username is too short"],
+    match: [/^[A-Za-z0-9]+$/, "Username need to be alphanumeric"],
     unique: true,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Password is required!"],
 
     match: [/^[A-Za-z0-9]+$/, "Password is not like required"],
     minLength: [8, "Min length password 8 characters"],
