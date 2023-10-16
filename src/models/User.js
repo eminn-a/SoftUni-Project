ab6f229f8a703c541978648c515eb50b09a4b2d7
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-10-16 17:09:30 +0300

    Add validation to register page (schema)

--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    minLength: [5, "Password is too short"],
+    match: /^[A-Za-z0-9]+$/,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+
+    match: [/^[A-Za-z0-9]+$/, "Password is not like required"],
+    minLength: [8, "Min length password 8 characters"],
+  },
 });
 
 userSchema.virtual("repeatPassword").set(function (value) {
